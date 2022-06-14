055672347aded1e66ded6cd0b61904bce21d93e0
Ghost_chu <2908803755@qq.com>
2022-06-14 23:05:57 +0800

    Also ignore the prograss, target, only keep the challenge

--- src/main/java/ru/armagidon/achievementignore/AchievementIgnore.java
+++ src/main/java/ru/armagidon/achievementignore/AchievementIgnore.java
@@ -10,7 +10,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -23,9 +22,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public final class AchievementIgnore extends JavaPlugin implements Listener {
 
@@ -39,7 +39,7 @@ public final class AchievementIgnore extends JavaPlugin implements Listener {
         saveDefaultConfig();
         getDataFolder().mkdirs();
         File file = new File(getDataFolder(), "data.yml");
-        if(!file.exists()){
+        if (!file.exists()) {
             try {
                 file.createNewFile();
             } catch (IOException e) {
@@ -49,43 +49,44 @@ public final class AchievementIgnore extends JavaPlugin implements Listener {
         storage = YamlConfiguration.loadConfiguration(file);
         deserialize();
         PluginCommand command = getCommand("ignoreachievements");
-        if(command!=null){
+        if (command != null) {
             command.setTabCompleter((sender, command1, alias, args) -> new ArrayList<>());
             command.setExecutor(this);
         }
 
-        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.CHAT) {
-            @Override
-            public void onPacketSending(PacketEvent event) {
-
-                if(!event.getPacketType().equals(PacketType.Play.Server.CHAT)) return;
-                WrappedChatComponent c = event.getPacket().getChatComponents().read(0);
-                if(c==null) return;
-                String json = c.getJson();
-                if(json==null) return;
-                JsonObject element = new JsonParser().parse(json).getAsJsonObject();
-                JsonElement trEl = element.get("translate");
-                if(trEl!=null&&trEl.getAsString().startsWith("chat.type.advancement")){
-                    JsonArray arr = element.getAsJsonArray("with");
-                    JsonObject o = arr.get(0).getAsJsonObject();
-                    String inse = o.get("insertion").getAsString();
-                    Player player = Bukkit.getPlayer(inse);
-
-                    Set<Player> recipients = Bukkit.getOnlinePlayers().stream().filter(p -> !ignore.contains(p.getName())).collect(Collectors.toSet());
-                    recipients.add(player);
-
-                    if(!recipients.contains(event.getPlayer())){
-                        event.setCancelled(true);
-                    }
-                }
+        ProtocolLibrary.getProtocolManager().addPacketListener(
+                new PacketAdapter(this, ListenerPriority.HIGHEST, PacketType.Play.Server.CHAT) {
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        if (!event.getPacketType().equals(PacketType.Play.Server.CHAT)) return;
+                        WrappedChatComponent c = event.getPacket().getChatComponents().read(0);
+                        if (c == null) return;
+                        String json = c.getJson();
+                        if (json == null) return;
+                        JsonObject element = new JsonParser().parse(json).getAsJsonObject();
+                        JsonElement trEl = element.get("translate");
+                        if (trEl == null)
+                            return;
+                        String tr = trEl.getAsString();
+                        if (tr != null && (tr.startsWith("chat.type.advancement") || tr.contains("has made the advancement") || tr.contains("has reached the goal"))) {
+                            JsonArray arr = element.getAsJsonArray("with");
+                            JsonObject o = arr.get(0).getAsJsonObject();
+                            String inse = o.get("insertion").getAsString();
+                            if (!inse.equals(event.getPlayer().getName())) {
+                                event.setCancelled(true);
+                            }
+//                    if(!recipients.contains(event.getPlayer())){
+//                        event.setCancelled(true);
+//                    }
+                        }
             }
         });
 
     }
 
-    private void deserialize(){
+    private void deserialize() {
 
-        if(storage!=null){
+        if (storage != null) {
             ignore.addAll(storage.getStringList("players"));
         }
 
@@ -99,24 +100,25 @@ public final class AchievementIgnore extends JavaPlugin implements Listener {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        ProtocolLibrary.getProtocolManager().removePacketListeners(this);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(!(sender instanceof Player)) return true;
+        if (!(sender instanceof Player)) return true;
         Player player = (Player) sender;
-        if(ignore.contains(player.getName())){
+        if (ignore.contains(player.getName())) {
             ignore.remove(player.getName());
-            String msg = getConfig().getString("unignore","unignore");
+            String msg = getConfig().getString("unignore", "unignore");
             sender.sendMessage(color.apply(msg));
         } else {
-            String msg = getConfig().getString("ignore","ignore");
+            String msg = getConfig().getString("ignore", "ignore");
             sender.sendMessage(color.apply(msg));
             ignore.add(player.getName());
         }
         return true;
     }
 
-    private Function<String, String> color = (s)-> ChatColor.translateAlternateColorCodes('&',s);
+    private Function<String, String> color = (s) -> ChatColor.translateAlternateColorCodes('&', s);
 
 }
